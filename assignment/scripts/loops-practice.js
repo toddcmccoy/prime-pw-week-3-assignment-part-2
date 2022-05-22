49d564ce1fea9764fe493bef9ba2639d3bac0d6c
Todd McCoy <tcmccoy@gmail.com>
2022-05-21 19:04:54 -0500

    finally fixed the for loop counting evennumbers.

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -27,10 +27,9 @@ for (let i=3; i<6; i++){
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
-for (let i=0; i<=11; i+2){
-  console.log(i);
+for (let number=2; number<=10; number=number+2){
+  console.log(number);
 }
-
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
@@ -53,6 +52,7 @@ console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
